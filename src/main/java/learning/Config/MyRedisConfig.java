28f0adac282a166772b0b8f31eb89f9bd2201b91
mzd_Ship <mzd@mail.ynu.edu.cn>
2019-10-29 21:35:32 +0800

    自定义序列化器  10/29

--- src/main/java/learning/Config/MyRedisConfig.java
+++ src/main/java/learning/Config/MyRedisConfig.java
@@ -2,11 +2,8 @@ package learning.Config;
 
 
 import learning.Bean.Employee;
-import org.apache.ibatis.ognl.ObjectElementsAccessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
